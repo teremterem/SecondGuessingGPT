eff07c4315cdcaf48a1b3dcb29d6b4c20cab5277
Oleksandr Tereshchenko <toporok@gmail.com>
2023-12-20 21:13:05 +0200

    gaia-first-try

--- try_gaia/single_question.py
+++ try_gaia/single_question.py
@@ -10,9 +10,15 @@ import warnings
 
 import pypdf
 from dotenv import load_dotenv
+from langchain import hub
+from langchain.agents import AgentExecutor, load_tools
+from langchain.agents.format_scratchpad import format_log_to_str
+from langchain.agents.output_parsers import ReActSingleInputOutputParser
+from langchain.tools.render import render_text_description
 
 load_dotenv()
 
+from langchain.chat_models import PromptLayerChatOpenAI
 import promptlayer
 
 # TODO Oleksandr: get rid of this warning suppression when PromptLayer doesn't produce "Expected Choice but got dict"
@@ -36,6 +42,34 @@ plain text unless specified otherwise.
 If you are asked for a comma separated list, apply the above rules depending of whether the element to be put in the \
 list is a number or a string.\
 """
+
+
+@forum.agent
+async def pdf_finder_agent(ctx: InteractionContext) -> None:
+    """Langchain based attempt."""
+    llm = PromptLayerChatOpenAI(temperature=0, model_name="gpt-4-1106-preview")
+    tools = load_tools(["serpapi"], llm=llm)
+    prompt = hub.pull("hwchase17/react")
+    prompt = prompt.partial(
+        tools=render_text_description(tools),
+        tool_names=", ".join([t.name for t in tools]),
+    )
+    llm_with_stop = llm.bind(stop=["\nObservation"])
+    agent = (
+        {
+            "input": lambda x: x["input"],
+            "agent_scratchpad": lambda x: format_log_to_str(x["intermediate_steps"]),
+        }
+        | prompt
+        | llm_with_stop
+        | ReActSingleInputOutputParser()
+    )
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, max_iterations=3)
+    agent_executor.invoke(
+        {
+            "input": (await ctx.request_messages.amaterialize_concluding_message()).content,
+        }
+    )
 
 
 @forum.agent
@@ -79,13 +113,14 @@ async def main() -> None:
     )
     print("\nQUESTION:", question)
 
-    assistant_responses = gaia_agent.quick_call(
-        question,
-        model="gpt-4-1106-preview",
-        # model="gpt-3.5-turbo-1106",
-        temperature=0,
-        stream=True,
-    )
+    # assistant_responses = gaia_agent.quick_call(
+    #     question,
+    #     model="gpt-4-1106-preview",
+    #     # model="gpt-3.5-turbo-1106",
+    #     temperature=0,
+    #     stream=True,
+    # )
+    assistant_responses = pdf_finder_agent.quick_call(question)
 
     async for response in assistant_responses:
         print("\n\033[1m\033[36mGPT: ", end="", flush=True)
